b1859a2ceed0892b5fcd6fa51c0f4779882ab2e3
Matthias Naber <naber.matthias+github@gmail.com>
2021-04-21 18:36:55 +0200

    fmt

--- examples/public-service/main.tf
+++ examples/public-service/main.tf
@@ -89,7 +89,7 @@ module "service" {
       backend_port     = 80
       backend_protocol = "HTTP"
       target_type      = "ip"
-      health_check     = {
+      health_check = {
         enabled = true
         path    = "/"
       }
@@ -99,8 +99,8 @@ module "service" {
   https_listener_rules = [{
     listener_arn = aws_lb_listener.http.arn
 
-    priority   = 42
-    actions    = [{
+    priority = 42
+    actions = [{
       type               = "forward"
       target_group_index = 0
     }]
@@ -111,25 +111,25 @@ module "service" {
 
   container_definitions = jsonencode([
     {
-      command: [
+      command : [
         "/bin/sh -c \"echo '<html> <head> <title>Hello from httpd service</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h1>Amazon ECS Sample App</h1> <h2>Congratulations!</h2> <p>Your application is now running on a container in Amazon ECS.</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""
       ],
-      cpu: 256,
-      entryPoint: ["sh", "-c"],
-      essential: true,
-      image: "httpd:2.4",
-      memory: 512,
-      name: local.service_name,
-      portMappings: [{
-        containerPort: 80
-        hostPort: 80
-        protocol: "tcp"
+      cpu : 256,
+      entryPoint : ["sh", "-c"],
+      essential : true,
+      image : "httpd:2.4",
+      memory : 512,
+      name : local.service_name,
+      portMappings : [{
+        containerPort : 80
+        hostPort : 80
+        protocol : "tcp"
       }]
     }
   ])
 
   ecr = {
-    image_tag_mutability         = "IMMUTABLE"
+    image_tag_mutability = "IMMUTABLE"
     image_scanning_configuration = {
       scan_on_push = true
     }
