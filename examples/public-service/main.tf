3333e17bf2cf69c898ada25965ce9af155d1f4b6
Moritz Zimmer <moritz.zmmr@gmail.com>
2022-11-08 13:05:59 +0100

    chore: fixed tflint warnings

--- examples/public-service/main.tf
+++ examples/public-service/main.tf
@@ -134,11 +134,4 @@ module "service" {
       }]
     }
   ])
-
-  ecr = {
-    image_tag_mutability = "IMMUTABLE"
-    image_scanning_configuration = {
-      scan_on_push = true
-    }
-  }
 }
--- examples/public-service/variables.tf
+++ examples/public-service/variables.tf
@@ -1,2 +1,3 @@
 variable "region" {
+  type = string
 }
