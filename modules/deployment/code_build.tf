4e5f779f66b8b95a5b47bf95d4539c7f82bd017b
Moritz Zimmer <moritzzimmer@users.noreply.github.com>
2022-12-14 13:11:17 +0100

    chore(deployment): updated default CodeBuild docker image (#85)

--- modules/deployment/code_build.tf
+++ modules/deployment/code_build.tf
@@ -34,7 +34,7 @@ resource "aws_codebuild_project" "this" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image        = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
     type         = "LINUX_CONTAINER"
 
     environment_variable {
