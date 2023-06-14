9559d3daef80abe3f54dc83230232df15a493e8c
Mark Ferrell <major@homeonderanged.org>
2023-06-14 05:57:27 -0700

    fix: ssm permissions for enable_execute_command (#113)

--- iam.tf
+++ iam.tf
@@ -52,3 +52,33 @@ data "aws_iam_policy_document" "ecs_task_assume_role_policy" {
     }
   }
 }
+
+data "aws_iam_policy_document" "enable_execute_command" {
+  count = var.enable_execute_command && var.task_role_arn == "" ? 1 : 0
+
+  statement {
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "enable_execute_command" {
+  count = var.enable_execute_command ? 1 : 0
+
+  name   = "enable-execute-command-${var.service_name}-${data.aws_region.current.name}"
+  path   = "/ecs/task-role/"
+  policy = data.aws_iam_policy_document.enable_execute_command[count.index].json
+}
+
+resource "aws_iam_role_policy_attachment" "enable_execute_command" {
+  count = var.enable_execute_command && var.task_role_arn == "" ? 1 : 0
+
+  role       = aws_iam_role.ecs_task_role[count.index].name
+  policy_arn = aws_iam_policy.enable_execute_command[count.index].arn
+}
